4e04ac9f9546f3709073d59892087e6ab0bd80b6
Bjoern Penning <bpenning@users.noreply.github.com>
2016-12-28 19:16:49 +0100

    Add files via upload
    reworked the extract values script entirely, given a modern sciplot version can also do the plotting

--- limits_contour/extract_values.py
+++ limits_contour/extract_values.py
@@ -24,65 +24,121 @@ def makePlot():
     infile = open(sys.argv[k+1],"r")
     filestoread=infile.readlines()
     yvalues=[]
+
+    #get from info file all yvalued (first column) and sort
     for i in range(len(filestoread)):
       thisline=filestoread[i].split()
       yvalues.append([float(thisline[0]),thisline[1]])
     yvalues.sort(key=lambda x: float(x[0]))
 
     #loop over y values getting tfiles and extracting x values and limits
-    medianvalues=[]
-    ybincenters=[]
-    xbincenters=[]
-
+    medianvalues=[]  #these are the median expectd mu values
+    xbins=[]
+    ybins=[]
+    
+    values = np.empty(shape=[0,3])
+    
     for i in range(len(yvalues)):
-      ybincenters.append(float(yvalues[i][0]))
-      tf = r.TFile(yvalues[i][1])
-      tree = tf.Get('limit')
-      xvalues=[]
+     # ybincenters.append(float(yvalues[i][0]))  #store yvalue in ybincenter
+      tf = r.TFile(yvalues[i][1]) #get the root files with x values per y value
+      tree = tf.Get('limit') #get limit branch
+      xvalues=[]  #will contain six values, observed (5) plus 5 expected (median (2) and +/- 1/2 sigma (0,1,3,4)), then repeating the pattern
       for j in range(tree.GetEntries()):
-        tree.GetEntry(j)
-        xvalues.append([tree.mh, tree.limit])
-        xvalues.sort(key=lambda x: float(x[0]))
+        tree.GetEntry(j)  
+        #this is awkard but best I could come up with without being mega error prone because of the way combine stores values
+        if ((j-2)%6==0):
+#          print str(j)+': '+str(yvalues[i][0])+'\t'+str(tree.mh)+'\t'+str(tree.limit) #in principle we have here all values
+          print '['+str(yvalues[i][0])+', '+str(tree.mh)+', '+str(tree.limit)+'],' #in principle we have here all values
+          xbins.append(tree.mh)
+          ybins.append(yvalues[i][0])
+          medianvalues.append(tree.limit)
+          values = np.append(values, [[tree.mh, yvalues[i][0], tree.limit]], axis=0)
+
+    values.view('i8,i8,i8').sort(order=['f2'], axis=0)  #sort according to last column (f0, f1, f2 etc)
+    #now we have all values in some sort of sensible numpy array
+#    print values
+
+    stops = [0.0000, 0.1250, 0.2500, 0.3750, 0.5000, 0.6250, 0.7500, 0.8750, 1.0000]
+    red   = [0.2082, 0.0592, 0.0780, 0.0232, 0.1802, 0.5301, 0.8186, 0.9956, 0.9764]
+    green = [0.1664, 0.3599, 0.5041, 0.6419, 0.7178, 0.7492, 0.7328, 0.7862, 0.9832]
+    blue  = [0.5293, 0.8684, 0.8385, 0.7914, 0.6425, 0.4662, 0.3499, 0.1968, 0.0539]
+
+    ered = []
+    egreen = []
+    eblue = []
+    for i, stop in enumerate(stops):
+      if i is 0:
+        ered.append( (stop, 0., red[i]) )
+        egreen.append( (stop, 0., green[i]) )
+        eblue.append( (stop, 0., blue[i]) )
+      elif i is len(stops)-1:
+        ered.append( (stop, red[i], 1.) )
+        egreen.append( (stop, green[i], 1.) )
+        eblue.append( (stop, blue[i], 1.) )
+      else:
+        ered.append( (stop, red[i], red[i]) )
+        egreen.append( (stop, green[i], green[i]) )
+        eblue.append( (stop, blue[i], blue[i]) )
+      cdict = {'red': ered, 'green': egreen, 'blue': eblue}
       
-        
-      xs = []
-      for item in xvalues:
-            if item[0] not in xs: xs.append(item[0])
+      bird = mcol.LinearSegmentedColormap('bird', cdict)
 
-      # get how many of them are there in the list
-      for x in xs:
-            check = [item for item in xvalues if x in item]
-            
-            if i==0:
-                  xbincenters.append(x)
-            
-      # get limits for all x values for this y value
-      for j in range(len(xvalues)):
-        if (j%6==0):
-          if len(xvalues)>j+2 : medianvalues.append([xvalues[j][0],yvalues[i][0],xvalues[j+2][1]])
-          else : medianvalues.append([xvalues[j][0],yvalues[i][0],10000])
-
-    graph_exp=r.TGraph2D()
+      exp=values
+#      xi, yi, zi = interp2(exp, 'cubic')
+#      plt.contour(xi, yi, zi, [1.0], colors='k')
+#      plt.contourf(xi, yi, zi, 200, cmap=bird)
+#      plt.colorbar()
 
 
-    xvalues=[]
-    yvalues=[]
-    values=[]
-    matrix=[]
-    
-    #Populate TGraph with points
-    n=0
-    for j in range(len(ybincenters)):
-      for i in range(len(xbincenters)):
-        print '['+str(xbincenters[i])+', ' +str(ybincenters[j])+', '+str(round(medianvalues[j*len(xbincenters)+i][2],4))+'],'
-        xvalues.append(xbincenters[i])
-        yvalues.append(ybincenters[j])
-        values.append(round(medianvalues[j*len(xbincenters)+i][2],4))
-        n=n+1
+#      plt.title('ms_50, cubic interpolation')
+#      plt.show()
+
+#      xi, yi, zi = interp2(exp, 'linear')
+#      plt.contour(xi, yi, zi, [1.0], colors='k')
+#      plt.contourf(xi, yi, zi, 200, cmap=bird)
+#      plt.colorbar()
+
+#      plt.title('ms_50, linear interpolation')
+#      plt.show()
 
 
-    exp = np.array((xvalues, yvalues, values), dtype=float)
-    
+      
+def interp(data, method='linear'):
+      x = data[:,0]
+      y = data[:,1]
+      z = data[:,2]
+      
+      xi = np.linspace(x.min(), x.max(), 1000)
+      yi = np.linspace(y.min(), y.max(), 1000)
+      zi = mlab.griddata(x, y, z, xi, yi, interp=method)
+      
+      return xi, yi, zi
+
+def interp2(data, method='linear'):
+      x = data[:,0]
+      y = data[:,1]
+      z = data[:,2]
+      
+      xi = np.linspace(x.min(), x.max(), 1000)
+      yi = np.linspace(y.min(), y.max(), 1000)
+      xi, yi = np.meshgrid(xi,yi)
+      zi = interpolate.griddata((x, y), z, (xi, yi), method=method)
+
+      return xi, yi, zi
+
+
+def interp3(data, method='linear'):
+      x = data[:,0]
+      y = data[:,1]
+      z = data[:,2]
+
+      xi = np.linspace(x.min(), x.max(), 1000)
+      yi = np.linspace(y.min(), y.max(), 1000)
+      xi, yi = np.meshgrid(xi,yi)
+      zi = interpolate.LinearNDInterpolator(x, y, z, method=method)(xi, yi)
+      
+      return xi, yi, zi
+                                
 makePlot()
 
  
